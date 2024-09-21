8d1f482d140ad2aa687eeaa01be667aa53796e59
Erdem <rifaterdemsahin@gmail.com>
2024-09-21 07:29:06 +0100

    Create kubectlcommand.sh

---
+++ Code/kubectlcommand.sh
@@ -0,0 +1,6 @@
+kubectl version --client
+kubectl get nodes
+kubectl create deployment my-nginx --image=nginx
+kubectl expose deployment my-nginx --port=80 --type=NodePort
+kubectl get pods
+ubectl get service my-nginx
