c05e17d65189680bbbcbbe61e8bad5ebc67be567
Erdem <rifaterdemsahin@gmail.com>
2024-09-21 10:04:39 +0100

    Update wslinstall.ps1

--- Code/wslinstall.ps1
+++ Code/wslinstall.ps1
@@ -1,3 +1,12 @@
+# Setup
+Install-WindowsFeature -Name Microsoft-Windows-Subsystem-Linux
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+wsl --set-default-version 2
+Add-AppxPackage .\Ubuntu_2004.2021.825.0_x64.appx
+wsl -l -v
+
+
+
 # Updates
 wsl --update
 wsl --list --verbose
