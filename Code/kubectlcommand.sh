7722bf89c4a24333e8a5dd626419db0685b6fa19
Erdem <rifaterdemsahin@gmail.com>
2024-09-21 07:30:36 +0100

    Update kubectlcommand.sh

--- Code/kubectlcommand.sh
+++ Code/kubectlcommand.sh
@@ -1,5 +1,10 @@
-kubectl version --client
+# Initial Starters
+kubectl version --client	
+kubectl config get-contexts
+
 kubectl get nodes
+
+# See one Container Working
 kubectl create deployment my-nginx --image=nginx
 kubectl expose deployment my-nginx --port=80 --type=NodePort
 kubectl get pods
