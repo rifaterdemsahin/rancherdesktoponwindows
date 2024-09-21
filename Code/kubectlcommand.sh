4d31fec92ad77d8293288ba8d9a1ad4025bafdb2
Erdem <rifaterdemsahin@gmail.com>
2024-09-21 12:30:32 +0100

    Update kubectlcommand.sh

--- Code/kubectlcommand.sh
+++ Code/kubectlcommand.sh
@@ -11,6 +11,8 @@ kubectl get pods
 ubectl get service my-nginx
 
 
-# Set WSL
+# Set WSL ( Be in the same network >>> powershell fails to get the curl but the wsl is closer > check the main architecture)
 export KUBECONFIG=/mnt/c/Users/<YourUsername>/.kube/config
 echo $KUBECONFIG
+kubectl get nodes
+kubectl config get-contexts
