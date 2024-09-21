3ab96a10739c73b800dd656692f221cfc9486262
Erdem <rifaterdemsahin@gmail.com>
2024-09-21 10:56:55 +0100

    Update chococommands.ps1

--- Code/chococommands.ps1
+++ Code/chococommands.ps1
@@ -1,2 +1,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install docker-desktop -y
+choco install notepadplusplus -y
+choco install vscode -y
