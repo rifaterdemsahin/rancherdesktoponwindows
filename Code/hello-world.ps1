0dbc71fce6cf033486e0c0f526a4e982a1c74625
Erdem <rifaterdemsahin@gmail.com>
2024-09-21 12:54:23 +0100

    Update hello-world.ps1

--- Code/hello-world.ps1
+++ Code/hello-world.ps1
@@ -4,7 +4,7 @@
    
    kubectl expose deployment hello-world --type=LoadBalancer --port=8080
    kubectl get svc
-
+   curl -vk http://<loadbalancer>:<8080>
 
 # If the EXTERNAL-IP is <pending> for too long:
 kubectl delete svc hello-world
