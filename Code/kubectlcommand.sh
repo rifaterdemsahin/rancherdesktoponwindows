e5f4697aca80631df9d151c3078f519f375ffc34
Erdem <rifaterdemsahin@gmail.com>
2024-09-21 12:29:30 +0100

    Update kubectlcommand.sh

--- Code/kubectlcommand.sh
+++ Code/kubectlcommand.sh
@@ -9,3 +9,8 @@ kubectl create deployment my-nginx --image=nginx
 kubectl expose deployment my-nginx --port=80 --type=NodePort
 kubectl get pods
 ubectl get service my-nginx
+
+
+# Set WSL
+export KUBECONFIG=/mnt/c/Users/<YourUsername>/.kube/config
+echo $KUBECONFIG
